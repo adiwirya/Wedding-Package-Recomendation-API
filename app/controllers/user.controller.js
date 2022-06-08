fe3bc8277d9ec1c8f7bbdfd0b03b885b9c396c9c
Adi Wirya <50317150+guardling7@users.noreply.github.com>
2022-06-09 02:07:34 +0700

    bindo

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -23,7 +23,7 @@ exports.register = async (req, res) =>  {
                 });
             }).catch((err) => {
                 res.status(500).json({
-                    message: err.message || "TRerjadi Kesalahan"
+                    message: err.message || "Terjadi Kesalahan"
                 });
             });
     } else {
